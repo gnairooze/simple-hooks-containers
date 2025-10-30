1fe079f12cf1e31929b2ff8eda063c7f079f096c
George Nairooze <george.nairooze@gmail.com>
2025-10-30 20:30:27 +0300

    fix the plugin path

--- dbs-setup/simplehooks-db/Data/03-ListenerType-Data.sql
+++ dbs-setup/simplehooks-db/Data/03-ListenerType-Data.sql
@@ -1,5 +1,5 @@
 use SimpleHooks
 go
 
-insert ListenerType(Id, [Name], [Path], CreateBy, ModifyBy, Notes) values (1, 'Anonymous', 'listener-plugins/Anonymous/SimpleHooks.ListenerPlugins.Anonymous.dll', 'system.admin', 'system.admin', '')
-insert ListenerType(Id, [Name], [Path], CreateBy, ModifyBy, Notes) values (2, 'TypeA', 'listener-plugins/TypeA/SimpleHooks.ListenerPlugins.TypeA.dll', 'system.admin', 'system.admin', 'include Bearer token in header')
+insert ListenerType(Id, [Name], [Path], CreateBy, ModifyBy, Notes) values (1, 'Anonymous', 'listener-plugins/anonymous/SimpleHooks.ListenerPlugins.Anonymous.dll', 'system.admin', 'system.admin', '')
+insert ListenerType(Id, [Name], [Path], CreateBy, ModifyBy, Notes) values (2, 'TypeA', 'listener-plugins/typea/SimpleHooks.ListenerPlugins.TypeA.dll', 'system.admin', 'system.admin', 'include Bearer token in header')
